1d14ed2827135b301b5186217d5717b643d6bc28
Valentin Quiedeville <hamilcare@gmail.com>
2018-06-27 21:42:33 +0200

    testci12

--- client/core/main.py
+++ client/core/main.py
@@ -4,27 +4,31 @@ from game import Game
 
 
 def main():
-    print("Appuyez sur n'importe quelle touche pour lancer la partie !")
+    try:
+        print("Appuyez sur n'importe quelle touche pour lancer la partie !")
 
-    pygame.init()
+        pygame.init()
+        
+        ecran = pygame.display.set_mode((800, 600))
 
-    ecran = pygame.display.set_mode((800, 600))
+        continuer = True
+        jeu = Game(ecran)
 
-    continuer = True
-    jeu = Game(ecran)
-
-    while continuer:
-        for event in pygame.event.get():
-            if event.type == const.QUIT or (event.type == const.KEYDOWN and event.key == const.K_ESCAPE):
-                # echap ou la croix
-                continuer = 0
-            if event.type == const.KEYDOWN:            
-                jeu.start()
+        while continuer:
+            for event in pygame.event.get():
+                if event.type == const.QUIT or (event.type == const.KEYDOWN and event.key == const.K_ESCAPE):
+                    # echap ou la croix
+                    continuer = 0
+                if event.type == const.KEYDOWN:            
+                    jeu.start()
 
 
-        pygame.display.flip()
+            pygame.display.flip()
 
-    pygame.quit()
+        pygame.quit()
+    except Error:
+        print(str(Error))
+        pygame.quit()
 
 
 if __name__ == '__main__':
